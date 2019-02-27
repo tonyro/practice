cea7f7bcfc7db52df2722ca8a7eb282060d0a92e
Anton Rovba <tony.rovba@gmail.com>
2019-02-26 16:15:43 -0800

    Completed First And Last Digit Sum challenge

---
+++ src/udemy/FirstAndLastDigitsSum.java
@@ -0,0 +1,27 @@
+package udemy;
+
+public class FirstAndLastDigitsSum {
+    public static void main(String[] args) {
+        System.out.println(sumFirstAndLastDigit(252));
+        System.out.println(sumFirstAndLastDigit(257));
+        System.out.println(sumFirstAndLastDigit(0));
+        System.out.println(sumFirstAndLastDigit(5));
+        System.out.println(sumFirstAndLastDigit(-10));
+    }
+
+    public static int sumFirstAndLastDigit(int number) {
+        if (number < 0) {
+            return -1;
+        }
+
+        int lastDigit = number % 10;
+        int firstDigit = 0;
+
+        while (number > 0) {
+            firstDigit = number;
+            number /= 10;
+        }
+
+        return firstDigit + lastDigit;
+    }
+}
