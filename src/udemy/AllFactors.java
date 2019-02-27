e7d4ec4aab2111d95ce2e11df6a0aa71ebf9e70f
Anton Rovba <tony.rovba@gmail.com>
2019-02-27 13:31:02 -0800

    Completed All Factors challenge

---
+++ src/udemy/AllFactors.java
@@ -0,0 +1,30 @@
+package udemy;
+
+public class AllFactors {
+    public static void main(String[] args) {
+        printFactors(6);
+        System.out.println("---------------------------------");
+        printFactors(32);
+        System.out.println("---------------------------------");
+        printFactors(10);
+        System.out.println("---------------------------------");
+        printFactors(-1);
+        System.out.println("---------------------------------");
+        printFactors(0);
+        System.out.println("---------------------------------");
+        printFactors(2);
+        System.out.println("---------------------------------");
+    }
+
+    public static void printFactors(int number) {
+        if (number < 1) {
+            System.out.println("Invalid Value");
+        } else {
+            for (int i = 1; i <= number; i++) {
+                if (number % i == 0) {
+                    System.out.println(i);
+                }
+            }
+        }
+    }
+}
