45a41a297d025fdf4174b216266606d3382afb29
Anton Rovba <tony.rovba@gmail.com>
2019-03-26 16:54:57 -0700

    Completed Point challenge

--- src/udemy/section6_oop_p1/Main.java
+++ src/udemy/section6_oop_p1/Main.java
@@ -3,6 +3,7 @@ package udemy.section6_oop_p1;
 public class Main {
     public static void main(String[] args) {
         // Simple Calculator
+        System.out.println("Simple Calculator");
         SimpleCalculator simpleCalculator = new SimpleCalculator();
         simpleCalculator.setFirstNumber(5.0);
         simpleCalculator.setSecondNumber(4);
@@ -26,6 +27,7 @@ public class Main {
         System.out.println();
 
         // Person
+        System.out.println("Person");
         Person person = new Person();
         person.setFirstName("");
         person.setLastName("");
@@ -46,6 +48,7 @@ public class Main {
         System.out.println();
 
         // Wall
+        System.out.println("Wall");
         Wall wall = new Wall(5, 4);
         System.out.println("area = " + wall.getArea());
         System.out.println("-------------------");
@@ -53,5 +56,20 @@ public class Main {
         System.out.println("width = " + wall.getWidth());
         System.out.println("height = " + wall.getHeight());
         System.out.println("area = " + wall.getArea());
+
+        System.out.println();
+        System.out.println("====================");
+        System.out.println();
+
+        // Point
+        System.out.println("Point");
+        Point first = new Point(6, 5);
+        Point second = new Point(3, 1);
+        System.out.println("distance(0,0) = " + first.distance());
+        System.out.println("distance(second) = " + first.distance(second));
+        System.out.println("distance(2,2) = " + first.distance(2, 2));
+        System.out.println("-------------------");
+        Point point = new Point();
+        System.out.println("distance() = "  + point.distance());
     }
 }
